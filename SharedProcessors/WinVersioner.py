a55e855f4e764ccc197d3574545e4bffcc20ff67
Alex Alequin <alex.alequin@uber.com>
2022-07-18 13:43:52 -0400

    linted and stuff.

--- SharedProcessors/WinVersioner.py
+++ SharedProcessors/WinVersioner.py
@@ -3,23 +3,22 @@
 See docstring for WinVersioner class
 """
 
-from subprocess import check_output, CalledProcessError
-import re
 import hashlib
-import platform
+import os
+import re
+from subprocess import CalledProcessError, check_output
 
 from autopkglib import (  # pylint: disable=import-error,unused-import
-    Processor,
-    ProcessorError,
-)
+    Processor, ProcessorError)
 
 __all__ = ["WinVersioner"]
 
 
 class WinVersioner(Processor):  # pylint: disable=too-few-public-methods
-    """
+    """Extracts an MSI or EXEs Metadata to gather version/control
+    panel displayname.
     Requires:
-    - https://github.com/Homebrew/linuxbrew-core/blob/master/Formula/msitools.rb
+    - brew install msitools
     - brew install exiftools
     """
 
@@ -27,14 +26,37 @@ class WinVersioner(Processor):  # pylint: disable=too-few-public-methods
     input_variables = {
         "pathname": {
             "required": True,
-            "description": "Full file path to gather data from."},
+            "description": "Full file path to gather data from.",
+        },
     }
     output_variables = {
-        "pkg_display_name": {"description": "the app name as it shows up in control panel"},
+        "pkg_display_name": {
+            "description": "the app name as it shows up in control panel"
+        },
         "pkg_version": {"description": "the verison of the app"},
         "pkg_sha256": {"description": "the sha256 value of the file"},
-        "pkg_file_extension": {"description": "why look it up again?"}
+        "pkg_file_extension": {"description": "why look it up again?"},
     }
+
+    def __init__(self):
+        """Defining vars we will eventually need"""
+        self.file_extension = None
+        self.binaries = {
+            "msi": {
+                "arm": "/opt/homebrew/bin/msiinfo",
+                "i386": "/usr/local/bin/msiinfo",
+            },
+            "exe": {
+                "arm": "/opt/homebrew/bin/exiftool",
+                "i386": "/usr/local/bin/exiftool",
+            },
+        }
+        self.binary = None
+        self.commands = {
+            "msi": [self.binary, "export", self.env["pathname"], "Property"],
+            "exe": [self.binary, self.env["pathname"]],
+        }
+        self.metadata = None
 
     def get_file_extension(self):
         """determine whether its an msi or exe"""
@@ -46,24 +68,24 @@ class WinVersioner(Processor):  # pylint: disable=too-few-public-methods
         output = self.run_cmd(command)
         if "Apple" in output:
             return "arm"
-        else:
-            return "i386"
+        return "i386"
 
     def get_sha256sum(self):
         """Returns the sha256 hash of a file specified."""
         sha256 = hashlib.sha256()
-        with open(self.env["pathname"], "rb") as f:
+        with open(self.env["pathname"], "rb") as pkg_file:
             chunk = None
             while chunk != b"":
-                chunk = f.read(1024)
+                chunk = pkg_file.read(1024)
                 sha256.update(chunk)
         return sha256.hexdigest()
 
     def get_version(self):
-        """gets the version respected by add/or remove programs in control panel"""
+        """gets the version respected by add/or remove
+        programs in control panel"""
         patterns = {
             "msi": r"\nProductVersion\t(.*)\r",
-            "exe": r"\nProduct Version\W*: (.*)\n"
+            "exe": r"\nProduct Version\W*: (.*)\n",
         }
         # Chrome does its own thing...
         pattern = patterns[self.file_extension]
@@ -80,51 +102,53 @@ class WinVersioner(Processor):  # pylint: disable=too-few-public-methods
         try:
             return matches.group(1)
         except IndexError:
-            raise ProcessorError(
-                "Look into Chrome metadata, not match found in comment containing version info"
+            raise ProcessorError(  # pylint: disable=raise-missing-from
+                (
+                    "Look into Chrome metadata, not match found in",
+                    " comment containing version info. Error:",
+                )
             )
 
     def get_display_name(self):
         """gets the installed (control panel) displayname"""
         patterns = {
             "msi": r"\nProductName\t(.*)\r",
-            "exe": r"\nProduct Name\W*: (.*)\n"
+            "exe": r"\nProduct Name\W*: (.*)\n",
         }
         pattern = patterns[self.file_extension]
         matches = re.search(pattern, self.metadata)
         try:
             return matches.group(1)
         except IndexError:
-            raise ProcessorError(f"We were not able to find {pattern} in provided metadata.")
+            raise ProcessorError(  # pylint: disable=raise-missing-from
+                (f"We were not able to find {pattern} in provided metadata.")
+            )
 
     def run_cmd(self, command):
-        """Runs a shell command and returns its output, returns None on failure"""
+        """Runs a shell command and returns its output,
+        returns None on failure"""
         try:
             return check_output(command).decode("utf-8")
         except CalledProcessError:
-            raise ProcessorError(f"Running of command: {command} has failed.")
+            raise ProcessorError(  # pylint: disable=raise-missing-from
+                f"Running of command: {command} has failed."
+            )
 
     def main(self):
         """gimme some main"""
         proc_arch = self.get_proper_arch()
         self.file_extension = self.get_file_extension()
-        # Dict that can be referred to via output of platform lib to get binary path.
-        self.binaries = {
-            "msi": {
-                "arm": "/opt/homebrew/bin/msiinfo",
-                "i386": "/usr/local/bin/msiinfo"
-            },
-            "exe": {
-                "arm": "/opt/homebrew/bin/exiftool",
-                "i386": "/usr/local/bin/exiftool"
-            }
-        }
         self.binary = self.binaries[self.file_extension][proc_arch]
-        # Build the appropriate command based on the file extension and generated vars.
-        self.commands = {
-            "msi": [self.binary, "export", self.env["pathname"], "Property"],
-            "exe": [self.binary, self.env["pathname"]]
-        }
+        # We know what binary we need, let's stop if it doesn't exist.
+        if not os.path.isfile(self.binary):
+            raise ProcessorError(
+                (
+                    f"FailedDeps: {self.binary} does not exist on disk.",
+                    " Please use homebrew to install it.",
+                )
+            )
+        # Build the appropriate command based on the file
+        # extension and generated vars.
         command = self.commands[self.file_extension]
         self.metadata = self.run_cmd(command)
         if self.metadata:
@@ -136,7 +160,10 @@ class WinVersioner(Processor):  # pylint: disable=too-few-public-methods
             else:
                 self.env["pkg_version"] = self.get_version()
         else:
-            raise ProcessorError(f"We were not able to produce metadata for {self.env['pathname']}")
+            raise ProcessorError(
+                ("We were not able to produce metadata for ", self.env["pathname"])
+            )
+
 
 if __name__ == "__main__":
     PROCESSOR = WinVersioner()
