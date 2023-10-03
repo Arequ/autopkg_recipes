5f93c967eed3a4890fc7c68f0aafab5b82e4c2d0
root <root@arequ-win>
2023-10-02 20:27:01 -0400

    lfs

--- WinSignatureVerify.py
+++ WinSignatureVerify.py
@@ -1,27 +1,3 @@
-Description: Downloads the latest version of Google Chrome Ebter (64-bit) for Windows
-Identifier: com.github.arequ.download.Chrome-Enterprise-Win-64
-MinimumVersion: '1.0.0'
-
-Input:
-  APP_FILENAME: GoogleChromeStandaloneEnterprise64.msi
-  SUBJECT: /C=US/ST=California/L=Mountain View/O=Google LLC/CN=Google LLC
-  DOWNLOAD_URL: https://dl.google.com/chrome/install/GoogleChromeStandaloneEnterprise64.msi
-  NAME: Google Chrome (64-bit)
-
-Process:
-- Processor: URLDownloaderPython
-  Arguments:
-    url: '%DOWNLOAD_URL%'
-    filename: '%APP_FILENAME%'
-
-- Processor: WinSignatureVerify
-  Arguments:
-    pathname: '%pathname%'
-    expected_subject: '%SUBJECT%'
-    expected_signature: '%SIGNATURE%'
-
-- Processor: EndOfCheckPhase
-
-- Processor: StopProcessingIf
-  Arguments:
-    predicate: 'download_changed == FALSE'
+version https://git-lfs.github.com/spec/v1
+oid sha256:8c2bd1a9e428850c303a3d5988c81baa96be2ff3cc478a351acc144b8369ece9
+size 808
